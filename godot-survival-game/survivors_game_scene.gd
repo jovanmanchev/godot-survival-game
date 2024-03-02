d033373051fe974887074e3a18343e6f141d53e1
jovanmanchev <jovanmanchev3003@gmail.com>
2024-03-02 15:32:06 +0100

    project files added

---
+++ godot-survival-game/survivors_game_scene.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+
+func spawn_emeny():
+	var new_enemy = preload("res://enemy.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_enemy.global_position = %PathFollow2D.global_position
+	add_child(new_enemy)
+	
+
+
+func _on_timer_timeout():
+	spawn_emeny()
+ 	
+
+
+func _on_player_health_depleted():
+	%GameOver.visible = true
+	get_tree().paused = true
